710a983d89a422dbddab8fb40f39ce66c5a9b61c
Chaek08 <chaek777zz@gmail.com>
2026-09-15 07:33:33 +0300

    наводим порядки

--- xr_3da/xrGame/HUDManager.cpp
+++ xr_3da/xrGame/HUDManager.cpp
@@ -232,7 +232,7 @@ void  CHUDManager::RenderUI()
 	if( Device.Paused() && bShowPauseString){
 		CGameFont* pFont	= Font().pFontGraffiti50Russian;
 		pFont->SetColor		(0x80FF0000	);
-		LPCSTR _str			= CStringTable().translate("Game paused").c_str();
+		LPCSTR _str			= CStringTable().translate("st_game_paused").c_str();
 		
 		Fvector2			_pos;
 		_pos.set			(UI_BASE_WIDTH/2.0f, UI_BASE_HEIGHT/2.0f);
