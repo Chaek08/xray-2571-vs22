5555f1b503854291b200653bb133d4146f02e5b0
Chaek08 <chaek777zz@gmail.com>
2026-09-21 21:06:37 +0300

    едрить калатить

--- xr_3da/xrGame/InventoryOwner.cpp
+++ xr_3da/xrGame/InventoryOwner.cpp
@@ -69,7 +69,8 @@ CInventoryOwner::~CInventoryOwner			()
 
 void CInventoryOwner::Load					(LPCSTR section)
 {
-	m_inventory->SetMaxWeight(READ_IF_EXISTS(pSettings, r_float, section, "inv_max_weight", 1000.f));
+	if (pSettings->line_exist(section, "inv_max_weight"))
+		m_inventory->SetMaxWeight(pSettings->r_float(section, "inv_max_weight"));
 }
 
 void CInventoryOwner::reload				(LPCSTR section)
